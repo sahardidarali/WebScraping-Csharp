db8b4a112da00f50f668c65f855929264d9fece8
Sahar Didar Ali <sahar.ali.28@live.com>
2021-12-15 23:58:36 +0500

    update code

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Automata_Updated_Project
 {
@@ -14,43 +11,55 @@ namespace Automata_Updated_Project
     {
         static void Main(string[] args)
         {
-            string mainUrl = "http://books.toscrape.com/catalogue/category/books/";
-            Console.WriteLine("Choose Category");
-            Console.WriteLine("Travel = 1 \nMystery = 2 \nHistorical Fiction = 3 \nSequential Art = 4 \nHistory = 5 ");
-            int category=Convert.ToInt32(Console.ReadLine());
-             switch (category)
+            dynamic d1 = new System.Dynamic.ExpandoObject();
+            var categoryLinks = new Dictionary<int, dynamic>();
+            string mainUrl = "https://books.toscrape.com/index.html";
+            HtmlDocument doc = GetDocument(mainUrl);
+            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//li/ul/li/a");
+            var baseUri = new Uri(mainUrl);
+             int key=0;
+            foreach (var link in linkNodes)
             {
-                case 1:
-                    mainUrl = mainUrl + "travel_2/";
-                    break;
-                case 2:
-                    mainUrl = mainUrl + "mystery_3/";
-                    break;
-                case 3:
-                    mainUrl = mainUrl + "historical-fiction_4/";
-                    break;
-                case 4:
-                    mainUrl = mainUrl + "sequential-art_5/";
-                    break;
-                case 5:
-                    mainUrl = mainUrl + "history_32/";
-                    break;
-                default:
-                    Console.WriteLine("Invalid");
-                    break;
+                categoryLinks[key] = d1;
+                string href = link.Attributes["href"].Value;
+                string val = link.InnerText.Trim();
+                var a_link = new Uri(new Uri(baseUri, href).AbsoluteUri);
+                string text = a_link.Segments.GetValue(4).ToString();
+                string[] textSplit = text.Split('_');
+                categoryLinks[key] = new {s1=textSplit[0], s2 = val, s3 = a_link.ToString() };
+                key++;
             }
-            var bookLinks = GetBookLinks(mainUrl);
-            Console.WriteLine("Found {0} links", bookLinks.Count);
-            //var books = GetBookDetails(bookLinks);
-            var books= GetBookDetailsArray(bookLinks);
-            for (int i = 0; i < books.GetLength(0); i++)
             {
-                for (int j = 0; j < books.GetLength(1); j++)
+                Console.WriteLine("Here are the categories");
+                foreach (KeyValuePair<int, dynamic> category in categoryLinks)
+                   Console.WriteLine("{0}", category.Value.s2);
+                Console.WriteLine("Enter Category Name");
+                string selectedCategory = Console.ReadLine().ToLower().Replace(" ", "-");
+                string categoryUrl = string.Empty;
+                foreach (KeyValuePair<int, dynamic> category in categoryLinks)
                 {
-                    Console.WriteLine("Books[{0},{1}] : " + books[i,j],i,j);
+                    if (selectedCategory == category.Value.s1)
+                    {
+                        categoryUrl = category.Value.s3;
+                        break;
+                    }
+
+                    
                 }
+
+                var bookLinks = GetBookLinks(categoryUrl);
+                Console.WriteLine("Found {0} links", bookLinks.Count);
+                ////var books = GetBookDetails(bookLinks);
+                var books = GetBookDetailsArray(bookLinks);
+                for (int i = 0; i < books.GetLength(0); i++)
+                {
+                    for (int j = 0; j < books.GetLength(1); j++)
+                    {
+                        Console.WriteLine("Books[{0},{1}] : " + books[i, j], i, j);
+                    }
+                }
+                //exportToCSV(books);
             }
-            //exportToCSV(books);
         }
         // Parses the URL and returns HtmlDocument object
         static HtmlDocument GetDocument(string url)
@@ -63,31 +72,38 @@ namespace Automata_Updated_Project
         //public HtmlNode SelectSingleNode(string xpath) { }
         static List<string> GetBookLinks(string mainUrl)
         {
-            string url = mainUrl + "index.html";
+            var uri = new Uri(mainUrl);
+            var noLastSegment = string.Format("{0}://{1}", uri.Scheme, uri.Authority);
+            for (int j = 0; j < uri.Segments.Length - 1; j++)
+            {
+                noLastSegment += uri.Segments[j];
+            }
             var bookLinks = new List<string>();
-            HtmlDocument doc = GetDocument(url);
+            HtmlDocument doc = GetDocument(mainUrl);
             HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//h3/a");
-            var baseUri = new Uri(url);
+            var baseUri = new Uri(mainUrl);
+
+            string href =string.Empty;
             foreach (var link in linkNodes)
             {
-                string href = link.Attributes["href"].Value;
+                 href = link.Attributes["href"].Value;
                 bookLinks.Add(new Uri(baseUri, href).AbsoluteUri);
             }
-            bookLinks = GetPageLinks(bookLinks,mainUrl,url);
+            bookLinks = GetPageLinks(bookLinks,mainUrl,noLastSegment);
 
             return bookLinks;
         }
-        static List<string> GetPageLinks(List<string> bookLinks, string mainUrl, string url)
+        static List<string> GetPageLinks(List<string> bookLinks, string mainUrl,string url)
         {
             string val;
-            HtmlDocument doc = GetDocument(url);
-            HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//h3/a");
+            HtmlDocument doc = GetDocument(mainUrl);
             string x = "//form/strong";
             int page = Convert.ToInt32(doc.DocumentNode.SelectSingleNode(x).InnerText);
-            if (page >20 && bookLinks.Count!=page)
+
+            if (page > 20 && bookLinks.Count != page)
             {
                 string href1 = doc.DocumentNode.SelectSingleNode("//ul[contains(@class,\"pager\")]/li[@class=\"next\"]/a").Attributes["href"].Value;
-                val = mainUrl + href1;
+                val = url + href1;
                 HtmlDocument doc1 = GetDocument(val);
                 HtmlNodeCollection nodes = doc1.DocumentNode.SelectNodes("//h3/a");
                 var baseUri1 = new Uri(val);
@@ -96,7 +112,7 @@ namespace Automata_Updated_Project
                     string href = node.Attributes["href"].Value;
                     bookLinks.Add(new Uri(baseUri1, href).AbsoluteUri);
                 }
-                GetPageLinks(bookLinks, mainUrl, val);
+                GetPageLinks(bookLinks, val,null);
             }
             return bookLinks;
         }
@@ -113,7 +129,6 @@ namespace Automata_Updated_Project
                 book.Title = document.DocumentNode.SelectSingleNode(titleXPath).InnerText;
                 book.Description = document.DocumentNode.SelectSingleNode(descriptionXPath).InnerText;
                 books.Add(book);
-                int[,] intarray = new int[4, 2];
             }
             return books;
         }
@@ -133,11 +148,18 @@ namespace Automata_Updated_Project
                     {
                         booksArray[i, j] = document.DocumentNode.SelectSingleNode(titleXPath).InnerText;
                     }
+                if (descriptionXPath == null)
+                {
+                    booksArray[i, j] = "No description";
+
+                }
+                else
+                {
                     booksArray[i, j] = document.DocumentNode.SelectSingleNode(descriptionXPath).InnerText;
                     i++;
                 }
+            }
             
-        
             return booksArray;
         }
         static void exportToCSV(List<Book> books)
